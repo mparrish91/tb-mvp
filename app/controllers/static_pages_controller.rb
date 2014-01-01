10b45c98d62a4de83773d87225913974b4777d82
malcolm <mparrish91@gmail.com>
2014-01-01 13:09:23 -0600

    add precompiled assets for Heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,42 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
+
+    if params[:health]
+      @feed_items = current_user.feed.paginate(page: :category params[:page])
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
+
+    if params[:fitness]
+      5.times { puts "Malcolm!!!" }
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
+
+    if params[:family]
+      5.times { puts "Malcolm!!!" }
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
+
+    if params[:religion]
+      5.times { puts "Malcolm!!!" }
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
+
+       if params[:humanrelationships]
+      5.times { puts "Malcolm!!!" }
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
+
+       if params[:leadership]
+      5.times { puts "Malcolm!!!" }
+    else
+      5.times { puts "No Malcolm!!!" }
+    end
   end
   
   def help
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ActiveRecord::Base
 
   default_scope -> { order('created_at DESC') }
   
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 300 }
   validates :user_id, presence: true
 
 
